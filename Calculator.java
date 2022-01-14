f6ad7920bfa1d231434cdd99f9404063fee49099
Melamory05 <97288524+Melamory05@users.noreply.github.com>
2022-01-14 14:02:53 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator {
+
+private String operation;
+private Scanner scanner;
+
+     public Calculator() {
+         this.scanner = new Scanner(System.in);
+     }
+     public void calculate() {
+        while (!"q".equals(operation)) {
+            try {
+
+                System.out.println (calculationStep());
+            }
+            catch (InputMismatchException ex){
+                System.out.println("Wrong input data!");
+            }
+        }
+    }
+
+    private double calculationStep() throws InputMismatchException {
+        System.out.println("enter first number:");
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("enter second number:");
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("enter operation (only '+' is available) to perform or q if you want to quit:");
+
+        operation = scanner.nextLine();
+
+
+        return performOperation(firstNumber, secondNumber);
+    }
+
+    private double performOperation(int firstNumber, int secondNumber) {
+        double result;
+
+        switch (operation){
+            case "+":
+                result = plus(firstNumber, secondNumber);
+                break;
+            case "-":
+                result = minus(firstNumber, secondNumber);
+                break;
+                case "/":
+result = divide(firstNumber, secondNumber);
+break;
+            case "*":
+                result = multiplication(firstNumber,secondNumber);
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        return result;
+    }
+
+    private int plus(int numberOne, int numberTwo){
+        return numberOne + numberTwo;
+
+    }
+    private int minus(int numberOne, int numberTwo){
+         return numberOne - numberTwo;
+    }
+
+private int divide(int numberOne, int numberTwo){
+         return numberOne / numberTwo;
+}
+private int multiplication(int numberOne, int numberTwo){
+         return numberOne * numberTwo;
+}
+}
